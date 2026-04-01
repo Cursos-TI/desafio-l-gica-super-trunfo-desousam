9c248b2c86ddca1248a22bf6158e30dc56fec9e8
de Sousa <mariliacs0239@gmail.com>
2026-04-01 11:49:59 +0000

    refazendo o codigo e atualizando as variaveis

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,102 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
     // ================ CARTA 1 ================
-
-    char estado1[3];
-    char codigo1[4];
-    char cidade1[50];
-    int populacao1;
-    float area1, pib1;
-    int pontos1;
+    char cidade1[50] = "Sao Paulo";
+    int populacao1 = 12345000;
+    float area1 = 1521.11, pib1 = 699.28;
+    int pontos1 = 50;
 
     float densidade1, pibPerCapita1;
 
-    printf("=== Cadastro da Carta 1 ===\n");
+    // ================ CARTA 2 ================
+    char cidade2[50] = "Rio de Janeiro";
+    int populacao2 = 6789000;
+    float area2 = 1200.25, pib2 = 300.50;
+    int pontos2 = 30;
 
-    printf("Estado: ");
-    scanf("%s", estado1);
+    float densidade2, pibPerCapita2;   
 
-    printf("Codigo: ");
-    scanf("%s", codigo1);
-    getchar();
-
-    printf("Nome da cidade: ");
-    scanf("%[^\n]", cidade1);
-    getchar();
-
-    printf("Populacao: ");
-    scanf("%d", &populacao1);
-
-    printf("Area: ");
-    scanf("%f", &area1);
-
-    printf("PIB: ");
-    scanf("%f", &pib1);
-
-    printf("Pontos turisticos: ");
-    scanf("%d", &pontos1);
-
-
-    // ================= CARTA 2 =================
-    char estado2[3];
-    char codigo2[4];
-    char cidade2[50];
-    int populacao2;
-    float area2, pib2;
-    int pontos2;
-
-    float densidade2, pibPerCapita2;
-
-    printf("\n=== Cadastro da Carta 2 ===\n");
-
-    printf("Estado: ");
-    scanf("%s", estado2);
-
-    printf("Codigo: ");
-    scanf("%s", codigo2);
-    getchar();
-
-    printf("Nome da cidade: ");
-    scanf("%[^\n]", cidade2);
-    getchar();
-
-    printf("Populacao: ");
-    scanf("%d", &populacao2);
-
-    printf("Area: ");
-    scanf("%f", &area2);
-
-    printf("PIB: ");
-    scanf("%f", &pib2);
-
-    printf("Pontos turisticos: ");
-    scanf("%d", &pontos2);
-
-
-    // ================= CÁLCULOS =================
-    densidade1 = populacao1 / area1;
-    pibPerCapita1 = pib1 / populacao1;
-
-    densidade2 = populacao2 / area2;
-    pibPerCapita2 = pib2 / populacao2;
-
-
-    // ================= COMPARAÇÃO =================
-    printf("\n=== Comparacao de Cartas===\n");
-    printf("Atributo escolhido: Populacao\n\n");
-
-    printf("Carta 1 - %s (%s): %d\n", cidade1, estado1, populacao1);
-    printf("Carta 2 - %s (%s): %d\n", cidade2, estado2, populacao2);
-
-    if (populacao1 > populacao2) {
-        printf("\nResultado: Carta 1 venceu!\n", cidade1);
-    } else if (populacao2 > populacao1) {
-        printf("\nResultado: Carta 2 venceu!\n", cidade2);
-    } else {
-        printf("\n Resultado: Empate\n");
-    }
-
-    return 0;
 }
